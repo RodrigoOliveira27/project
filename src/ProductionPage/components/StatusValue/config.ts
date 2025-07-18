df07aa02c6a465ffab59a0634898810f3ecd6ac1
Rodrigo Oliveira <36684@aedonamaria.pt>
2025-07-18 16:00:06 +0100

    Production

--- src/ProductionPage/components/StatusValue/config.ts
+++
@@ -1,7 +0,0 @@
-import { EStatus } from "../../../types";
-
-export const STATUS_COLOR_MAPPING: { [key in EStatus]: string } = {
-    [EStatus.Success]: "green",
-    [EStatus.Error]: "red",
-    [EStatus.Warning]: "yellow",
-}
--- src/ProductionPage/components/StatusValue/types.ts
+++
@@ -1,5 +0,0 @@
-export enum EStatus{
-    Success,
-    Error,
-    Warning
-}
